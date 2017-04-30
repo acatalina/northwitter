3f943a4ed30154d7592fd76c6910f826ceb36335
acatalina <accarrasco@gmail.com>
2017-04-30 16:49:19 +0100

    add ROOT

--- spec/App.test.js
+++ spec/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../src/components/App';
+const ROOT = 'https://protected-oasis-31937.herokuapp.com';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App ROOT={ROOT}/>, div);
 });
